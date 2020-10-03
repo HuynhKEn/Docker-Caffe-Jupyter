999848e0d8c42c24587bbcb056d22252b89c5b91
HuynhKEn <40224237+HuynhKEn@users.noreply.github.com>
2020-10-04 03:09:10 +0700

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,8 +1,9 @@
 c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False
-c.NotebookApp.port = 5000
+
 
 # setting up the password
 from IPython.lib import passwd
 password = passwd("000")
 c.NotebookApp.password = password
+c.NotebookApp.port = 8888
